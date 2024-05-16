2491624f64ea01343c7b1534b62fa9b053d38a73
Ebenezer Veeraraju <ebenezer.veeraraju@ibm.com>
2024-05-16 22:56:25 +0530

    added wait time before frame capture for neural network

--- src/index_page.py
+++ src/index_page.py
@@ -12,6 +12,7 @@ screw_coordinates = []
 screwdriver_tag_id = 3
 start_time = 0
 screw_index = 0
+wait_time = 0
 results = []
 
 
@@ -33,13 +34,13 @@ def get_template():
 
 
 def screwing_process(frame):
-    global screw_coordinates, screwdriver_tag_id, start_time, screw_index
+    global screw_coordinates, screwdriver_tag_id, start_time, screw_index, wait_time
     if not screw_coordinates:
         # Display message that no template has been selected
         cv2.putText(frame, "Template Error, No Screws Sequence Found", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
                     (0, 255, 0), 2, cv2.LINE_AA)
         return frame
-    raw_frame = frame.copy()
+    raw_frame = frame
     screwdriver_center = np.array([0, 0])
     at_detector = apriltag.Detector()
     # Process image:
@@ -64,7 +65,8 @@ def screwing_process(frame):
     nearest_screw = np.argmin(screwdriver_to_screw)
     if screw_index < len(screw_coordinates):
         # 4. Display information and circles, in green 
-        cv2.putText(frame, "Next Screw : " + str(screw_index + 1), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2,
+        cv2.putText(frame, "Next Screw : " + str(screw_index + 1), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0),
+                    2,
                     cv2.LINE_AA)
         # Display the type of the next screw
         cv2.putText(frame, "Next Screw Type : " + screw_coordinates[screw_index][2], (10, 60), cv2.FONT_HERSHEY_SIMPLEX,
@@ -77,15 +79,16 @@ def screwing_process(frame):
         # Display the nearest screw number
         cv2.putText(frame, "Nearest Screw : " + str(nearest_screw + 1), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
                     (0, 255, 0), 2, cv2.LINE_AA)
-    # check if the nearest screw is the next screw to be screwed
+        # check if the nearest screw is the next screw to be screwed
         if nearest_screw == screw_index:
             # Draw a circle at the nearest screw
-            cv2.circle(frame, (screw_coordinates[nearest_screw][0], screw_coordinates[nearest_screw][1]), 8, (0, 255, 0), 8)
+            cv2.circle(frame, (screw_coordinates[nearest_screw][0], screw_coordinates[nearest_screw][1]), 8,
+                       (0, 255, 0), 8)
             # check if the screwdriver is close enough to the screw
             if screwdriver_to_screw[nearest_screw] < 30:
                 # draw an orange circle on the screw to indicate screwing
                 frame = cv2.circle(frame, (screw_coordinates[nearest_screw][0], screw_coordinates[nearest_screw][1]),
-                                radius=8, color=(0, 166, 255), thickness=8)
+                                   radius=8, color=(0, 166, 255), thickness=8)
                 # check if start_time has been initialized
                 if start_time == 0:
                     start_time = time.time()
@@ -105,26 +108,34 @@ def screwing_process(frame):
             # reset start_time
             start_time = 0
             # Draw a circle at the nearest screw
-            cv2.circle(frame, (screw_coordinates[nearest_screw][0], screw_coordinates[nearest_screw][1]), 8, (0, 0, 255), 8)
+            cv2.circle(frame, (screw_coordinates[nearest_screw][0], screw_coordinates[nearest_screw][1]), 8,
+                       (0, 0, 255), 8)
 
         # display green circle on screws that have been screwed
         for i in range(screw_index):
             frame = cv2.circle(frame, (screw_coordinates[i][0], screw_coordinates[i][1]), 8,
-                            (255, 255, 255), 8)
+                               (255, 255, 255), 8)
     else:
         # break the loop and display message that all screws have been screwed
         # ask to move screwdriver away
         if len(results) == 0:
             if screwdriver_to_screw[nearest_screw] < 200:
                 cv2.putText(frame, "Move screwdriver away", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2,
-                        cv2.LINE_AA)
+                            cv2.LINE_AA)
+                wait_time = 0
             else:
-                #delay
-                time.sleep(2)
-                for index, screw in enumerate(screw_coordinates):
-                    # check if screw is present using neural network
-                    status = verify_screw_presence(raw_frame, screw[0], screw[1])
-                    results.append(status)
+                if wait_time == 0:
+                    wait_time = time.time()
+                else:
+                    elapsed_time = time.time() - wait_time
+                    cv2.putText(frame, "Capturing Frame", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2,
+                                cv2.LINE_AA)
+                    if elapsed_time > 3:
+                        wait_time = 0
+                    for index, screw in enumerate(screw_coordinates):
+                        # check if screw is present using neural network
+                        status = verify_screw_presence(raw_frame, screw[0], screw[1])
+                        results.append(status)
         else:
             print("Screw Verification Results:", results)
             cv2.putText(frame, "Screw Verification Results:", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2,
