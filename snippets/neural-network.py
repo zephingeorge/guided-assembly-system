c1f05335de998c83712be1398703a4f3483a9fb7
Ebenezer Veeraraju <ebenezer.veeraraju@ibm.com>
2024-05-16 21:52:48 +0530

    updating neural network processing

--- snippets/neural-network.py
+++ snippets/neural-network.py
@@ -9,47 +9,15 @@ print(keras.__version__)
 print(tf.__version__)
 
 print("Loading model")
-model = keras.models.load_model('./neural-network/model.keras')
+model = keras.models.load_model('../neural-network/model.keras')
 print(model.__class__)
 print("Model loaded")
 
 # Initialize the video capture
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(1)
 
 # Initialize global variables for mouse coordinates
 mouseX, mouseY = -1, -1
-
-
-def predict_image(input_image):
-    # Resize the image to 128, 128
-    input_image = tf.image.resize(input_image, (128, 128))
-    # Call predict on the model
-    output = model.predict(np.expand_dims(input_image, axis=0))
-    output = output > 0.5
-    return output
-
-
-# Function to calculate cropped frame size and offset
-def get_crop_info(frame_shape, click_point, crop_size):
-    # Extract frame height and width
-    frame_height, frame_width = frame_shape[:2]
-
-    # Calculate half crop size for centering
-    half_crop_size = crop_size // 2
-
-    # Ensure click point is within frame boundaries
-    click_point_x = max(0, min(click_point[0], frame_width - 1))
-    click_point_y = max(0, min(click_point[1], frame_height - 1))
-
-    # Calculate top-left corner coordinates for cropping
-    top_left_x = max(0, click_point_x - half_crop_size)
-    top_left_y = max(0, click_point_y - half_crop_size)
-
-    # Calculate bottom-right corner coordinates for cropping
-    bottom_right_x = min(frame_width, click_point_x + half_crop_size)
-    bottom_right_y = min(frame_height, click_point_y + half_crop_size)
-
-    return top_left_x, top_left_y, bottom_right_x, bottom_right_y
 
 
 # Callback function for handling double-click events
@@ -62,25 +30,35 @@ def draw_circle(event, x, y, flags, param):
         # Check if frame is available
         if frame is not None:
             # Define crop size (adjust as needed)
-            crop_size = 16
-
-            # Get cropping information
-            top_left_x, top_left_y, bottom_right_x, bottom_right_y = get_crop_info(frame.shape, (mouseX, mouseY),
-                                                                                   crop_size)
-
-            # Extract cropped frame
+            crop_size = 20
+            frame_height, frame_width, _ = frame.shape
+            half_crop_size = crop_size // 2
+            click_point_x = max(0, min(x, frame_width - 1))
+            click_point_y = max(0, min(y, frame_height - 1))
+            top_left_x = max(0, click_point_x - half_crop_size)
+            top_left_y = max(0, click_point_y - half_crop_size)
+            bottom_right_x = min(frame_width, click_point_x + half_crop_size)
+            bottom_right_y = min(frame_height, click_point_y + half_crop_size)
             cropped_frame = frame[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
-
-            # Resize cropped frame to 64x64 (adjust interpolation method if needed)
-            resized_frame = cv2.resize(cropped_frame, (64, 64), interpolation=cv2.INTER_AREA)
-            result = predict_image(resized_frame)
-            if result:
-                print("------------Screw detected------------")
+            cropped_frame_rgb = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
+            resized_frame = cv2.resize(cropped_frame_rgb, (64, 64), interpolation=cv2.INTER_AREA)
+            screw_patch = tf.image.resize(resized_frame, (128, 128))
+            prediction = model.predict(np.expand_dims(screw_patch, axis=0))
+            print('prediction: ', prediction)
+            predicted_class = np.argmax(prediction, axis=1)
+            print('predicted class: ', predicted_class)
+            if predicted_class[0] == 0:
+                print("m4")
+            elif predicted_class[0] == 1:
+                print("m8")
+            elif predicted_class[0] == 2:
+                print("m12")
+            elif predicted_class[0] == 3:
+                print("none")
             else:
-                print("-----------No screw detected----------")
-            # Define desired filename and path (replace with your preference)
+                print("unknown")
             filename = f"cropped_image_{mouseX}_{mouseY}.jpg"
-            filepath = "./neural-network/screw-extracts/" + filename
+            filepath = "../neural-network/screw-extracts/" + filename
             print(f"Image : {filepath}")
             cv2.imwrite(filepath, resized_frame)
 
